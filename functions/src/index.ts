9e44dc2e89c6efac7ac3117a756783490adcaefb
Brian <brianbaso@gmail.com>
2020-04-05 17:16:43 -0400

    add config, deploy google cloud function

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,18 @@
 import * as functions from 'firebase-functions';
+import * as admin from 'firebase-admin';
+import * as express from 'express';
+import * as bodyParser from "body-parser";
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+admin.initializeApp(functions.config().firebase);
+
+const app = express();
+const main = express();
+
+main.use('/api/v1', app);
+main.use(bodyParser.json());
+
+export const webApi = functions.https.onRequest(main);
+
+app.get('/warmup', (request, response) => {
+    response.send('Warming up friend.');
+})
